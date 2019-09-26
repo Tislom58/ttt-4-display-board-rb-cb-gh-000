d80ac233c32ad0f93ec87b14f5cdf55b132b9351
Daniel Müller <danmuller.astro@gmail.com>
2019-09-26 14:14:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,8 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
   puts " #{board[3]} | #{baord[4]} | #{board[5]}"
+  puts "-----------"
   puts " #{board[6]} | #{baord[7]} | #{board[8]}"
 end
