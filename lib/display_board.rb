bb1cabddea7eb7fa16645de20e0589563ea878ab
Daniel Müller <danmuller.astro@gmail.com>
2019-09-26 15:34:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(board)
-  board([" "," "," "," "," "," "," "," "," "])
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
   puts " #{board[3]} | #{baord[4]} | #{board[5]}"
   puts "-----------"
   puts " #{board[6]} | #{baord[7]} | #{board[8]}"
 end
-display_board
+display_board([" "," "," "," "," "," "," "," "," "])
+display_board(board) 
